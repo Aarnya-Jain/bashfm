5ca659fc3bdd5d71c3f0d16b64a5478c90cf8878
Aarnya Jain <aarnyajain417@gmail.com>
2025-11-15 20:43:15 +0530

    Added Media Support using ffmpeg and chafa

--- src/main.sh
+++ src/main.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-# --- Globals ---
+# all the global vars
 max_items=0   # Will be set by get_terminal_size
 list=()       # The array of files in the current dir
-cur_list=()   # A backup of the list
+cur_list=()   # A backup of the list to use in search function
 list_total=0  # The index of the last item in the list
 
 scroll_index=0        # Index in the main 'list' array (0 to list_total)
 window_start_index=0  # 'list' index currently at the top of the screen
 cursor_y=0            # Visual cursor row on screen (0 to max_items-1)
 previous_index=0      # To remember the previous dir index
+
+search_term="" # to hold the search keyword
+hidden_files_show_mode=0  # var to toggle hidden files ( 0 = off by default )
+
+# Adding a config file for key bindings and theme - colors
 
 
 setup_terminal() {
@@ -40,20 +45,60 @@ reset_terminal() {
     stty echo
 }
 
-clear_screen() {
-    # Only clear the scrolling window (dir item list).
-    # '\e[%sH':    Move cursor to bottom of scroll area.
-    # '\e[9999C':  Move cursor to right edge of the terminal.
-    # '\e[1J':     Clear screen to top left corner (from cursor up).
-    # '\e[2J':     Clear screen fully (if using tmux) (fixes clear issues).
-    # '\e[1;%sr':  Clearing the screen resets the scroll region(?). Re-set it.
-    #              Also sets cursor to (0,0).
-    printf '\e[%sH\e[9999C\e[1J%b\e[1;%sr' "$((LINES-2))" "${TMUX:+\e[2J}" "$max_items"
+search_mode() {
+
+    search_term=""
+    local k
+
+    while true; do
+
+        local filtered_list=()
+
+        # using backed up cur_list
+        for item in "${cur_list[@]}"; do
+            local filename=${item##*/}
+            if [[ "$filename" == *"${search_term}"* ]]; then
+                filtered_list+=("$item")
+            fi
+        done
+
+        list=("${filtered_list[@]}")
+        [[ -z ${list[0]} ]] && list[0]="empty"
+        ((list_total=${#list[@]} - 1))
+
+        scroll_index=0
+        window_start_index=0
+        cursor_y=0
+        draw_screen
+
+        # draw the search over the status bar
+        printf "\e[$((LINES-1));0H\e[J" # Go to bottom line & clear everything below ( \e[J )
+        printf "\e[48;5;214m\e[30m%s\n" "Enter esc to escape search mode ..."
+        printf "\e[48;5;214m\e[30m/%s\e[0m" "$search_term" # ( theme change will be here too )
+
+        read -s -n 1 k
+
+        case "$k" in
+            "") # ENTER: confirm search
+                break
+                ;;
+            $'\e') # ESCAPE: cancel search
+                # Restore the master list
+                list=("${cur_list[@]}")
+                ((list_total=${#list[@]} - 1))
+                break
+                ;;
+            $'\177'|$'\b') # BACKSPACE
+                # removing the last character
+                search_term=${search_term%?}
+                ;;
+            *) # Any other key
+                # add the key to the search term
+                search_term+="$k"
+                ;;
+        esac
+    done
 }
-
-# search_mode() {
-
-# }
 
 open() {
   local selected_item=$1
@@ -66,6 +111,7 @@ open() {
       scroll_index=0         # Reset cursor
       window_start_index=0
       cursor_y=0
+      search_term=""
       draw_screen            # Re-draw
 
   # if the seletced is a file
@@ -73,6 +119,9 @@ open() {
 
       # getting only the name of the file
       local filename=${selected_item##*/}
+
+      local quoted_path
+      printf -v quoted_path "%q" "$selected_item" # Make a shell safe path for audio Logic
 
       case "$selected_item" in
         # text files
@@ -115,31 +164,88 @@ open() {
                 else
                     reset_terminal
                     nvim "$selected_item"
-                    setup_terminal # Re-enter TUI mode
-                    draw_screen    # Re-draw
+                    setup_terminal
+                    draw_screen
                 fi
                 ;;
 
         # media files ( uses chafa )
-        *.png|*.jpg|*.jpeg|*.gif|*.bmp|*.mp4)
+        *.png|*.jpg|*.jpeg|*.gif|*.bmp)
               if [[ -n "$ZELLIJ_SESSION_NAME" ]]; then
 
                   zellij action new-pane --name "$filename" --close-on-exit -- \
                       $SHELL -c "chafa '$selected_item'; read -n 1 -s -r -p 'Press any key to close...'"
               else
-                  # Fallback: open in current terminal
-                  reset_terminal
+                  setup_terminal # to open a new buffer again
                   chafa "$selected_item"
                   read -n 1 -s -r -p 'Press any key to close...'
+                  reset_terminal # closed the new buffer
                   setup_terminal
                   draw_screen
               fi
               ;;
 
-        *)
-        if [[ -n "$ZELLIJ_SESSION_NAME" ]]; then
+        *.mp3|*.wav|*.flac|*.ogg|*.m4a|*.aac)
+            local i
+            if [[ -n "$ZELLIJ_SESSION_NAME" ]]; then
                 zellij action new-pane --name "$filename" --close-on-exit -- \
-                        $SHELL -c 'nvim "$1"; exit' "pane-shell" "$selected_item"
+                    $SHELL -c "
+
+                        printf '\e[H\e[2J';
+                        printf 'File: %s\n' '$filename';
+
+                        # Get width inside the new shell
+                        read -r _ w < <(stty size); ((w -= 4));
+
+                        # Run ffmpeg (errors are NOT hidden now)
+                        ffmpeg -hide_banner -loglevel error -i $quoted_path -filter_complex \"showwavespic=s=\${w}x120:colors=fire\" -frames:v 1 -f image2pipe -v quiet - | chafa -f symbols -;
+
+                        printf '\nPlaying... (Ctrl^C to quit)\n'; # temporary
+
+                        # Run ffplay (errors are NOT hidden now)
+                        ffplay -hide_banner -loglevel error -autoexit -nodisp $quoted_path < /dev/tty; # ffplay blocks user input while running so we give direct access to keyboard for getting q
+
+                        exit;
+                    "
+            else
+                setup_terminal
+
+                printf '\e[H\e[2J';
+                printf 'File: %s\n' "$filename";
+
+                # Get width inside the new shell
+                read -r _ w < <(stty size); ((w -= 4));
+
+                ffmpeg -hide_banner -loglevel error -i $quoted_path -filter_complex "showwavespic=s="$w"x120:colors=fire" -frames:v 1 -f image2pipe -v quiet - | chafa -f symbols -;
+
+                printf '\nPlaying... (Ctrl^C to quit)\n'; # temporary
+
+                ffplay -hide_banner -loglevel error -autoexit -nodisp $quoted_path < /dev/tty;
+
+                reset_terminal
+                setup_terminal
+                draw_screen
+            fi
+            ;;
+
+        # using ffplay for videos
+        *.mp4|*.mkv|*.mov|*.avi|*.webm)
+              if [[ -n "$ZELLIJ_SESSION_NAME" ]]; then
+
+                  zellij action new-pane --name "$filename" --close-on-exit -- ffplay -autoexit "$selected_item"
+              else
+                  setup_terminal
+                  ffplay -autoexit "$selected_item"
+                  reset_terminal
+                  setup_terminal
+                  draw_screen
+              fi
+              ;;
+
+        # will open any other file in neovim
+        *)
+            if [[ -n "$ZELLIJ_SESSION_NAME" ]]; then
+                zellij action new-pane --name "$filename" --close-on-exit -- $SHELL -c 'nvim "$1"; exit' "pane-shell" "$selected_item"
             else
                 reset_terminal
                 nvim "$selected_item"
@@ -156,19 +262,26 @@ open() {
 
 get_terminal_size() {
   read -r LINES COLUMNS < <(stty size)
-  max_items=$((LINES - 3)) # Reserve 3 lines for status/header
+  max_items=$((LINES - 3)) # reserving 3 lines for status
 }
 
 read_current_directory() {
+
+  if (( hidden_files_show_mode == 1 )); then
+      shopt -s dotglob # setting dotglob , showing the hidden files in *
+  else
+      shopt -u dotglob # unsetting dotglob , hiding the hidden files in *
+  fi
+
   local dirs=() files=() item_index=0 path
 
   path=$PWD
+  # removing / if root directory for neatness
   [[ $path == / ]] && path=
 
   for item in "$path"/*; do
       if [[ -d $item ]]; then
           dirs+=("$item")
-          # We'll use this later to remember cursor position
           [[ $item == "$OLDPWD" ]] && ((previous_index=item_index))
           ((item_index++))
       else
@@ -180,31 +293,31 @@ read_current_directory() {
   [[ -z ${list[0]} ]] && list[0]="empty"
   ((list_total=${#list[@]} - 1))
 
+  cur_list=("${list[@]}")
+
 }
 
 draw_status_line() {
 
-    printf "\e[${LINES};0H" # Move to bottom row
-    printf "\e[K"           # Clear the Line
-    # Background: \e[48;5;214m
-    # Foreground (black): \e[30m
-    # Reset: \e[0m
+    printf "\e[${LINES};0H" # move to bottom row
+    printf "\e[K"           # clear the Line
+    # background: \e[48;5;214m
+    # foreground (black): \e[30m
+    # reset: \e[0m
     printf "\e[48;5;214m\e[30m \e[7mSelected: ${list[scroll_index]##*/} ($((scroll_index+1))/$((list_total+1)))\e[m \e[0m"
-
-    # printf "\e[48;5;214m\e[30m Selected: ${list[scroll_index]##*/} ($scroll_index/$list_total) \e[0m"
 }
 
 
 draw_screen() {
-    # Clear the screen (the cause of our flicker)
+
     printf '\e[H\e[2J'
 
-    # --- Draw the File List (The Window) ---
+
     for(( i=0; i<max_items; i++)); do
-        # Get the real data index from our main 'list'
+        # Getting the real data index from our list
         local data_index=$(( window_start_index + i ))
 
-        # Stop if we're past the end of the real list
+        # Stopping if we are past the end of the real list
         (( data_index > list_total )) && break
 
         local item_path="${list[data_index]}"
@@ -213,8 +326,7 @@ draw_screen() {
         local item_name="${item_path##*/}"
         [[ -d "$item_path" ]] && item_name+="/"
 
-        # --- The New Highlight Logic ---
-        # Check if the visual row 'i' matches our cursor's visual row 'cursor_y'
+        # Checking if the visual row i matches our cursor's visual row cursor_y
         if (( i == cursor_y )); then
             printf '\e[48;5;214m\e[30m%s\e[0m\n' "$item_name"
         else
@@ -227,45 +339,42 @@ draw_screen() {
 
 
 function main {
-  # Trap resize events: get new size and redraw
+  # trapping resize events: getting new size and redraw
   # SIGWINCH is the signal for terminal resize
   trap 'get_terminal_size; draw_screen' SIGWINCH
 
-  # Run all setup
+
+ # setting up
   setup_terminal
   get_terminal_size
-
-
-  # 1. Load data ONCE at the start
   read_current_directory
-
-  # 2. Draw screen ONCE at the start
   draw_screen
 
   for ((;;)); do
-      # 3. Wait for a key
-      read -s -n 1 k
+      # getting user input ( used timeouts to avoid blocks )
+      read -s -n 1 -t 0.1 k
 
       if [[ $k == $'\e' ]]; then
-        read -rsn1 k2
-        read -rsn1 k3
+        read -rsn1 -t 0.01 k2
+        read -rsn1 -t 0.01 k3
         k="$k$k2$k3"
       fi
 
-      # 4. Handle the key
+
+
+      # handling the key
+      if [[ -n "$k" ]]; then
       case "$k" in
-          q) # Quit
+          q) # quit
               break ;;
 
           j|$'\e[B') # SCROLL DOWN
               if (( scroll_index < list_total )); then
-                  ((scroll_index++)) # Move data cursor down
+                  ((scroll_index++)) # move data cursor down
 
                   if (( cursor_y < (max_items - 1) )); then
-                      # --- Case 1: Just move visual cursor ---
                       ((cursor_y++))
                   else
-                      # --- Case 2: At bottom, scroll window ---
                       ((window_start_index++))
                   fi
                   draw_screen
@@ -274,72 +383,88 @@ function main {
 
           k|$'\e[A') # SCROLL UP
               if (( scroll_index > 0 )); then
-                  ((scroll_index--)) # Move data cursor up
+                  ((scroll_index--)) # move data cursor up
 
                   if (( cursor_y > 0 )); then
-                      # --- Case 1: Just move visual cursor ---
                       ((cursor_y--))
                   else
-                      # --- Case 2: At top, scroll window ---
                       ((window_start_index--))
                   fi
                   draw_screen
               fi
               ;;
 
-          l|$'\e[C') # OPEN (l = right)
+          l|$'\e[C') # OPEN
               open "${list[scroll_index]}"
               ;;
 
-          h|$'\e[D') # GO BACK (h = left)
+          h|$'\e[D') # GO BACK
               cd ..
               read_current_directory # Re-read data
-              # 1. Set our target data position
+              search_term=""
+
               scroll_index=$previous_index
 
-              # 2. Safety check
+              # safety check
               # If the index is out of bounds (e.g., file deleted), reset to 0
-              (( scroll_index > list_total )) && scroll_index=0
 
-              # 3. --- Apply "Smooth Scroll" Centering Logic ---
+              (( scroll_index > list_total )) && scroll_index=0
 
               # Calculate the "middle" of the screen
               local half_window=$(( max_items / 2 ))
 
               if (( scroll_index < half_window )); then
-                  # --- CASE 1: Near the TOP ---
+                  # case1 near the TOP
                   # The item is in the top-half of the first page.
                   # Just set the window to the top.
                   window_start_index=0
                   cursor_y=$scroll_index
 
               elif (( (list_total - scroll_index) < half_window )); then
-                  # --- CASE 2: Near the BOTTOM ---
+                  # case2 near the BOTTOM
                   # The item is in the bottom-half of the last page.
-                  # "Pin" the window to the bottom.
+                  # PIN the window to the bottom.
                   window_start_index=$(( list_total - max_items + 1 ))
 
-                  # Safety check: don't let window_start go below 0
-                  # (This happens if the total list is smaller than max_items)
+                  # don't let window_start go below 0
+                  # This happens if the total list is smaller than max_items
                   (( window_start_index < 0 )) && window_start_index=0
 
                   # The cursor is the offset from the new window start
                   cursor_y=$(( scroll_index - window_start_index ))
 
               else
-                  # --- CASE 3: In the MIDDLE ---
-                  # This is the "true" smooth scroll.
+                  # case3 in the MIDDLE
                   # Center the window around the scroll_index.
                   window_start_index=$(( scroll_index - half_window ))
                   cursor_y=$half_window
               fi
-              draw_screen            # Re-draw
+              draw_screen
               ;;
 
+        '/')
+              search_mode
+              search_term=""
+              draw_screen
+              ;;
+
+        '.')
+            ((hidden_files_show_mode = !hidden_files_show_mode))
+            read_current_directory
+            scroll_index=0
+            window_start_index=0
+            cursor_y=0
+            draw_screen
+            ;;
+
       esac
+      fi
+
+    [[ -t 1 ]] || exit 1
+
   done
 
-  # 5. Cleanup
+# cleaning up the terminal to original state
   reset_terminal
 }
 
